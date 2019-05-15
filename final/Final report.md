422aa171a5925e6430c8076426cd2710fe363822
yudingyi333 <46979199+yudingyi333@users.noreply.github.com>
2019-05-15 18:22:07 -0500

    Final report

--- final/Final report.md
+++ final/Final report.md
@@ -1,5 +1,5 @@
 
-#Final project
+#Final project#
 
 
 ## 1.Motivation
@@ -81,6 +81,8 @@ some other interesting correlation: communityAverage has a negative correlation 
 We decided to cluster all the houses into five categorize.
 And we plot several images to show the characteristics of the five different categorize.
 
+
+
 ![](https://i.imgur.com/AaHoXh7.png)
 
 From the graph, we can see clearly that cluster 5 are the larger house and cluster 2 are high price house relatively.
@@ -94,9 +96,12 @@ This graph is more interesting because we found that the more followers the high
 
 Thus, we think that for different types of houses, the factors that affect their prices are also different.
 
+We found that every time you rerun the coding, the color and the number of each clusters changed but the five types don't actually change. Thus we need give each group some labels.
+
 We use PCA method to cluster the house based on the features without the tradeTime. 
 Still, We divided all the houses into five categories. 
 
+ 
 
 Cluster 1 
 "buildingType" "district" "Lat" "communityAverage" "price" "fiveYearsProperty" "subway"            "followers" "ladderRatio" "Lng"              
@@ -130,4 +135,48 @@ In lm5 model, we found that Lng, Lat, followers, drawingRoom, bathRoom, Rnovatio
 
 **From the five simple linear model, we can tell it is true that  the price of house in different categories will increase by different reasones. Thus, in our price forecasting, we need adjust this situation to see which kind of house will perform better if we decided to invest**
 
-### 3.3 
+### 3.3 model prediction
+**The following result is based on the first run and we found that each time the cluster's number change, thus, there may be some inconsistent data. However, The cluster 4 we mentioned under is the cluster two we use above, we named them the expensive house cluster**
+
+We build models to analyze the relation of price and other variables for each cluster. 
+Firstly, we split each cluster data into two sets. If the trade time is before 2017, data will go into the train data set. If the trade time is after 2017, data will go into the test data set.
+Secondly, we use forward linear regression to test the relation to get the best model of each cluster. 
+Thirdly, using this model to check the test set to get rmse. The rmse is smaller, the better the model fit the dataset which means better predict the relation of price and the variables. After testing all five clusters, we get the rmse for cluster 1 is 24203.75, for cluster 2 is 30082.05, for cluster 3 is 23822.85, for expensive house cluster is 21503.17, for cluster 5 is 26454.4. In this case, the model for cluster 4 best fits the data.
+We use this model to get the predicted price which is compared with the actual price.  If the predicted price is higher than the actual price, this house is worth investing because the trade price is underestimated which means it has lot of room for the price to get higher in the future. In this case, we could sell it at a higher price than other houses.
+For example, as for the expensive house cluster, the best model predicted by “forward method is
+
+![](https://i.imgur.com/bD0c5vh.png)
+
+And its relevant coefficient of variables are:
+
+![](https://i.imgur.com/egUfYiJ.png)
+
+From the coefficient of variables, the biggest coefficient variable is “fiveYearsProperty” and this variable shows that if the owner have the property for less than 5 years, the price of the house would be decreased, this may because people do not like to live in “high turnover rate house”, they may think it must have some reasons for the residence to give up the house due to its specific characteristics, such as the location of the house, the neighbor of the house or even maybe the “bad history” of the house, the second largest coefficient is subway, and its coefficient is negative, usually the coefficient for the subway should be positive. But why is the situation?
+
+And this may because in expensive house cluster, the price of the house is commonly very high as the graph shows. And for rich people, they will not usually or even most of them would never take public transportation like bus and subway, thus they do not need to the subway near their house. Adversely, the house which is near the subway is crowded, and this will annoy the residence. So this maybe the reason why cluster 2’s subway coefficient is negative. The above illustration does not consider the interaction term. Still includes subway included as an example. Interaction term of ladder ratio and subway, the coefficient of the variable is positive, mean when there is subway, the price will change by coefficient*ladder ratios + subway coefficient + ladder* ladder coefficient. 
+
+In order to use the “pricing model”, it is important to collect the variables data and substituted the data in this model to see what is the house price which is ran by the model. So can give the house a price in value, to see if the house price now is understated or overstated. And there is one thing important, to find according cluster model to predict, for example, cluster4 model, it has high price character, so it needs to find the house price which is more than 10000 rmb/ square. So for example, if the after substituting the data, the price predicted by the cluster4 model is 15,000 rmb/ square, and the price now is 17,000 rmb, then it is not valuable to buy the house.
+
+
+##4 Conclusion
+As our data shows above, we found that the expensive house has the most accurate price forecast which means that if you give me the characteristics of a luxury real estate, we could have the best prediction of it. That is to say the more expensive house, the better its price stay increasing trend because the inflation and the lower risk it faces.
+
+![](https://i.imgur.com/kdtr5ow.png)
+
+As the graph shows, the most expensive house are located in Chaoyang district which has the most yellow points. If you want buy a house in Beijing, our suggestion is buy the house in Chaoyang.
+
+It is interesting to see that there's a missing piece of data in the middle. Houses in city center should be more expensive. At first, we think something wrong with our data. It took us sometime to remember that it is forbidden City in the middle.
+
+##5 What problem we have or need to improve
+
+Firstly, because we work in groups of three, each memebers have different clusters. Thus there are a lot of misrepresentations about the number of clusters. We tried our best to find every mistake and correct them. 
+
+Secondly, we know that the models we get has some disadvantages, it cannot consider the inflation factor and macro policy factors, because such macro factors will influence the price level overall, rather than individual house price. And also, under this method, the model is required to include a lot of “predictive” variables, in this case the model can predict some houses which has huge different price than the model predicted but the model actually is wrong. For example, if the government is going to build a school near some houses, the price of the house will definitely increase, which the model cannot catch.
+
+Thirdly, we should try to see that how our prediction of price changes over time, which will give us the effect in time series. And we should use other method to improve the accuracy to predict the house price, such as regression and classification trees or KNN. However, we do not have enough time for all of them.
+
+
+
+
+
+
--- final/final.R
+++ final/final.R
@@ -3,6 +3,8 @@ library(mosaic)
 library(foreach)
 library(doMC)
 library(ggplot2)
+library(dbplyr)
+library(lubridate)
 
 summary(beijing.house.price)
 
@@ -26,6 +28,8 @@ clust1$center[4,]*sigma + mu
 qplot(square,price, data=beijing.house.price, col=factor(clust1$cluster))
 qplot(buildingType,price, data=beijing.house.price, col=factor(clust1$cluster))
 qplot(followers,price, data=beijing.house.price, col=factor(clust1$cluster))
+qplot( price,subway, data=beijing.house.price, color=factor(clust1$cluster))
+qplot( price, data=beijing.house.price, color=factor(clust1$cluster))
 
 pc2 = prcomp(M, rank=5)
 
@@ -141,78 +145,185 @@ test_data1 = X1 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
 
 lm1 = lm(price ~ 1, data=train_data1)
 
+##split into 2 groups
 
+#X1 
+train_data1 = X1 %>% filter (as.numeric (substr(tradeTime,1,4))<2017)
+test_data1 = X1 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
+lm1 = lm(price ~ 1, data=train_data1)
 
-#7463
-
-lm_forward = step(lm1, direction='forward',
-                  
-                  scope=~(DOM + followers + square+
-                            
-                            livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
-                          
-                          + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty
-                          
-                          +subway + district + communityAverage)^2)
-
-
-
-lm1medium = lm(price~DOM + followers + square+
-                 
-                 livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
-               
-               + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty
-               
-               +subway + district + communityAverage,data=train_data1)
-
-#AIC=7382
-
-lm_step = step(lm1medium, 
-               
-               scope=~(DOM + followers + square+
-                         
-                         livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
-                       
-                       + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty
-                       
-                       +subway + district + communityAverage)^2)
+#Aic=120424.3
+lm_forward1 = step(lm1, direction='forward',
+                   scope=~(DOM + followers + square+
+                             livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
+                           + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty+communityAverage+subway)^2)
 
 
 
-
-
-lmX1=lm(price ~ DOM + followers + square + livingRoom + drawingRoom + 
-          
-          bathRoom + buildingType + constructionTime + renovationCondition + 
-          
-          buildingStructure + ladderRatio + elevator + fiveYearsProperty + 
-          
-          district + communityAverage + ladderRatio:communityAverage + 
-          
-          square:elevator + renovationCondition:communityAverage + 
-          
-          bathRoom:buildingStructure + followers:renovationCondition + 
-          
-          buildingStructure:ladderRatio + constructionTime:ladderRatio + 
-          
-          drawingRoom:buildingStructure + bathRoom:renovationCondition + 
-          
-          followers:square + followers:elevator + DOM:constructionTime + 
-          
-          livingRoom:elevator + livingRoom:constructionTime + followers:ladderRatio + 
-          
-          buildingType:communityAverage + buildingType:elevator + DOM:square + 
-          
-          DOM:followers,data=train_data1)
-
-
+lmX1=lm(price ~ communityAverage + renovationCondition + DOM + fiveYearsProperty + 
+          followers + square + elevator + ladderRatio + constructionTime + 
+          buildingType + buildingStructure + DOM:followers + DOM:square + 
+          communityAverage:renovationCondition + DOM:fiveYearsProperty + 
+          communityAverage:fiveYearsProperty + renovationCondition:followers + 
+          communityAverage:followers + renovationCondition:fiveYearsProperty + 
+          renovationCondition:square + communityAverage:ladderRatio + 
+          square:constructionTime + followers:square + communityAverage:square + 
+          fiveYearsProperty:constructionTime + square:elevator + square:ladderRatio + 
+          followers:constructionTime + elevator:buildingType + communityAverage:constructionTime + 
+          fiveYearsProperty:ladderRatio + ladderRatio:constructionTime + 
+          renovationCondition:constructionTime + fiveYearsProperty:elevator + 
+          DOM:elevator + followers:buildingType + elevator:constructionTime + 
+          fiveYearsProperty:square + communityAverage:buildingType + 
+          constructionTime:buildingType + communityAverage:buildingStructure + 
+          communityAverage:elevator + constructionTime:buildingStructure + 
+          buildingType:buildingStructure + elevator:buildingStructure + 
+          DOM:constructionTime + renovationCondition:DOM,data=train_data1)
 
 yhat_1 = predict(lmX1, test_data1)
-
 rmse = function(y, yhat) {
-  
   sqrt( mean( (y - yhat)^2 ) )
-  
 }
-
 rmse(test_data1$price, yhat_1)
+#rmse24203.75
+
+
+#X2
+train_data2 = X2 %>% filter (as.numeric (substr(tradeTime,1,4))<2017)
+test_data2 = X2 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
+lm2 = lm(price ~ 1, data=train_data2)
+
+#7463
+lm_forward2 = step(lm2, direction='forward',
+                   scope=~(DOM + followers + square+
+                             livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
+                           + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty++communityAverage+subway)^2)
+
+##7434.91
+lmX2=lm(price ~ communityAverage + constructionTime + DOM + fiveYearsProperty + 
+          renovationCondition + square + elevator + ladderRatio + subway + 
+          buildingType + DOM:renovationCondition + constructionTime:square + 
+          square:elevator + communityAverage:renovationCondition + 
+          communityAverage:ladderRatio + communityAverage:fiveYearsProperty + 
+          constructionTime:ladderRatio + square:ladderRatio + elevator:buildingType + 
+          communityAverage:buildingType + fiveYearsProperty:elevator + 
+          constructionTime:DOM + ladderRatio:buildingType,data=train_data2)
+
+yhat_2 = predict(lmX2, test_data2)
+rmse(test_data2$price, yhat_2)
+#rmse30082.05
+
+# X3
+train_data3 = X3 %>% filter (as.numeric (substr(tradeTime,1,4))<2017)
+test_data3 = X3 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
+lm3 = lm(price ~ 1, data=train_data3)
+
+lm_forward3 = step(lm3, direction='forward',
+                   scope=~(DOM + followers + square+
+                             livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
+                           + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty++communityAverage+subway)^2)
+
+#446402.5
+lmX3=lm(price ~ communityAverage + renovationCondition + DOM + fiveYearsProperty + 
+          followers + square + livingRoom + drawingRoom + buildingStructure + 
+          elevator + kitchen + subway + bathRoom + communityAverage:renovationCondition + 
+          DOM:followers + communityAverage:followers + renovationCondition:followers + 
+          DOM:fiveYearsProperty + communityAverage:square + renovationCondition:DOM + 
+          communityAverage:DOM + followers:livingRoom + DOM:livingRoom + 
+          renovationCondition:fiveYearsProperty + DOM:drawingRoom + 
+          DOM:square + renovationCondition:drawingRoom + fiveYearsProperty:livingRoom + 
+          communityAverage:buildingStructure + communityAverage:drawingRoom + 
+          renovationCondition:livingRoom + communityAverage:elevator + 
+          square:livingRoom + livingRoom:drawingRoom + renovationCondition:square + 
+          square:elevator + fiveYearsProperty:drawingRoom + drawingRoom:buildingStructure + 
+          followers:drawingRoom + livingRoom:kitchen + DOM:subway + 
+          communityAverage:subway + livingRoom:subway + livingRoom:elevator + 
+          communityAverage:fiveYearsProperty + communityAverage:livingRoom + 
+          DOM:buildingStructure + followers:buildingStructure + followers:square + 
+          fiveYearsProperty:subway + followers:elevator + square:bathRoom + 
+          communityAverage:bathRoom + fiveYearsProperty:bathRoom + 
+          livingRoom:bathRoom + renovationCondition:bathRoom + kitchen:bathRoom + 
+          elevator:bathRoom + DOM:bathRoom,data=train_data3)
+
+yhat_3 = predict(lmX3, test_data3)
+rmse(test_data3$price, yhat_3)
+#23822.85
+
+# X4
+train_data4 = X4 %>% filter (as.numeric (substr(tradeTime,1,4))<2017)
+test_data4 = X4 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
+lm4 = lm(price ~ 1, data=train_data4)
+
+lm_forward4 = step(lm4, direction='forward',
+                   scope=~(DOM + followers + square+
+                             livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
+                           + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty++communityAverage+subway)^2)
+#258917.3
+lmX4=lm(price ~ communityAverage + renovationCondition + DOM + fiveYearsProperty + 
+          square + followers + ladderRatio + subway + livingRoom + 
+          kitchen + buildingStructure + communityAverage:DOM + communityAverage:renovationCondition + 
+          DOM:followers + renovationCondition:followers + DOM:fiveYearsProperty + 
+          renovationCondition:DOM + square:ladderRatio + communityAverage:followers + 
+          communityAverage:livingRoom + DOM:livingRoom + communityAverage:subway + 
+          communityAverage:ladderRatio + square:livingRoom + ladderRatio:livingRoom + 
+          DOM:ladderRatio + ladderRatio:subway + renovationCondition:fiveYearsProperty + 
+          fiveYearsProperty:followers + renovationCondition:ladderRatio + 
+          followers:livingRoom + renovationCondition:kitchen + fiveYearsProperty:subway + 
+          communityAverage:buildingStructure + DOM:buildingStructure + 
+          renovationCondition:buildingStructure + communityAverage:square + 
+          subway:buildingStructure,data=train_data4)
+
+yhat_4 = predict(lmX4, test_data4)
+rmse(test_data4$price, yhat_4)
+#21503.17
+
+
+#X5
+train_data5 = X5 %>% filter (as.numeric (substr(tradeTime,1,4))<2017)
+test_data5 = X5 %>% filter (as.numeric (substr(tradeTime,1,4))>=2017)
+lm5 = lm(price ~ 1, data=train_data5)
+
+lm_forward5 = step(lm5, direction='forward',
+                   scope=~(DOM + followers + square+
+                             livingRoom + drawingRoom + kitchen + bathRoom +buildingType 
+                           + constructionTime + renovationCondition + buildingStructure +ladderRatio + elevator+ fiveYearsProperty++communityAverage+subway)^2)
+#168636.5
+lmX5=lm(price ~ communityAverage + DOM + renovationCondition + fiveYearsProperty + 
+          square + buildingStructure + followers + constructionTime + 
+          drawingRoom + livingRoom + elevator + buildingType + bathRoom + 
+          subway + ladderRatio + communityAverage:DOM + DOM:fiveYearsProperty + 
+          communityAverage:renovationCondition + DOM:renovationCondition + 
+          DOM:followers + square:constructionTime + fiveYearsProperty:followers + 
+          renovationCondition:fiveYearsProperty + fiveYearsProperty:square + 
+          DOM:square + renovationCondition:followers + communityAverage:followers + 
+          communityAverage:fiveYearsProperty + communityAverage:square + 
+          DOM:buildingStructure + communityAverage:constructionTime + 
+          fiveYearsProperty:constructionTime + renovationCondition:buildingStructure + 
+          square:drawingRoom + square:livingRoom + buildingStructure:constructionTime + 
+          buildingStructure:elevator + constructionTime:elevator + 
+          communityAverage:elevator + renovationCondition:buildingType + 
+          communityAverage:subway + livingRoom:bathRoom + followers:drawingRoom + 
+          fiveYearsProperty:bathRoom + buildingType:subway + elevator:ladderRatio + 
+          buildingStructure:buildingType + constructionTime:ladderRatio + 
+          buildingType:ladderRatio + square:elevator + livingRoom:elevator + 
+          livingRoom:subway + DOM:subway + communityAverage:buildingStructure + 
+          fiveYearsProperty:ladderRatio + buildingStructure:followers + 
+          communityAverage:livingRoom + communityAverage:bathRoom + 
+          buildingStructure:bathRoom + followers:bathRoom,data=train_data5)
+
+yhat_5 = predict(lmX5, test_data5)
+rmse(test_data5$price, yhat_5)
+#26454.4
+
+options(scipen = 100)
+coefficients (lmX4)
+
+which(clust1$cluster == 2)
+
+BeijingLoc <- data.frame('Long'=116.4075,'Lat' = 39.904)
+beijing.house.price %>% ggplot(aes(x=Lng,y=Lat)) + geom_point(aes(color=price),size=1,alpha=.5)  + 
+  scale_colour_gradientn(colors = viridis::viridis(100)) + theme_minimal() + 
+  theme(legend.position = 'bottom') + 
+  guides(color = guide_colorbar(barwidth = 35, barheight = .75)) +
+  geom_point(data=BeijingLoc,aes(x=Long,y=Lat),size=2,color='black') +
+  theme(axis.title= element_blank(), axis.text = element_blank()) +
+  labs(title='Average home price in Beijing geographically') + coord_fixed(1.3)
